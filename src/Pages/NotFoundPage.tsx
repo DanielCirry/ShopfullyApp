a2ea504b6bfa16c68f06ae5c06f3b9b28d387c84
DanielCirry <rudi1cd@gmail.com>
2020-05-17 23:32:47 +0100

    Adds not Found text

--- src/Pages/NotFoundPage.tsx
+++ src/Pages/NotFoundPage.tsx
@@ -2,6 +2,11 @@ import React from "react";
 
 export default class NotFound extends React.Component<any, any> {
   render() {
-    return <></>;
+    return (
+      <>
+        404Not Found. The resource you were looking for doesn't exist or has
+        been deleted. Please go back to Home
+      </>
+    );
   }
 }
